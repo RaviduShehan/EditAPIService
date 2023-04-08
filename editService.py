f38ecdf8497bdf9d4fc4869e9dbbd4d6b74e163f
Ravidu Perera <sheahn20133@gmail.com>
2023-04-08 22:54:15 +0530

    Add files

---
+++ editService.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import openai
+
+app = Flask(__name__)
+
+# Load OpenAI API key
+openai.api_key = "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"
+
+@app.route('/edit')
+def edit():
+    prompt = request.args.get('prompt')
+    if not prompt:
+        return jsonify(error="Prompt parameter is missing"), 400
+
+    try:
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=prompt,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.5
+        )
+        return jsonify(response=response.choices[0].text.strip())
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5003)
