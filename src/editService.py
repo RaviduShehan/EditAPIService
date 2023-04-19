10f1c67f2462a034bfb0b2d264165dc9ae7eb544
Ravidu Perera <sheahn20133@gmail.com>
2023-04-19 21:44:52 +0530

    Add manifest files and details

--- src/editService.py
+++ src/editService.py
@@ -1,18 +1,42 @@
+from datetime import datetime
+import os
 from flask import Flask, request, jsonify
 import openai
-import os
+from google.cloud import firestore
+import firebase_admin
+from firebase_admin import credentials
+
+
+
+#setup firebase credentials
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 
 # Load OpenAI API key
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
-@app.route('/edit')
+# Initialize Firestore client with explicit project ID
+project_id = "apiservices-384019"
+db = firestore.Client(project=project_id)
+
+@app.route('/')
 def edit():
+    # Add service name, status, and timestamp to Firestore
+    service_ref = db.collection('Services').document('EditService_Status')
+    print("Edit Service starting...")
+    service_data = {
+        'service_name': 'Edit',
+        'status': 'Starting',
+        'timestamp': datetime.now()
+    }
+    service_ref.set(service_data)
     prompt = request.args.get('prompt')
     if not prompt:
+        service_ref.update({'status': 'Empty prompt'})
         return jsonify(error="Prompt parameter is missing"), 400
-
+    service_ref.update({'status': 'Running'})
     try:
         response = openai.Completion.create(
             engine="davinci",
@@ -24,6 +48,8 @@ def edit():
         )
         return jsonify(response=response.choices[0].text.strip())
     except Exception as e:
+        # Update service status to 'error' if an exception occurs
+        service_ref.update({'status': 'Error'})
         return jsonify(error=str(e)), 500
 
 if __name__ == '__main__':
